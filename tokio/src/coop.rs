c731d6adaf97915f9638969645cb3525080c7195
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2022-08-15 09:24:44 +0200

    fix limits

--- tokio/src/coop.rs
+++ tokio/src/coop.rs
@@ -52,7 +52,7 @@ impl Budget {
     /// Note that as more yield points are added in the ecosystem, this value
     /// will probably also have to be raised.
     const fn initial() -> Budget {
-        Budget(Some(128))
+        Budget(Some(512))
     }
 
     /// Returns an unconstrained budget. Operations will not be limited.
